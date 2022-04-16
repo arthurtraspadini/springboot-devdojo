5e014252c5f43a1e65e871c211a243ded5100ed7
Arthur Teixeira Traspadini <arthurtraspadini@hotmail.com>
2022-04-16 08:15:05 -0300

    adicionado endpoint list no contexto animes
    Aula 00

---
+++ src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
@@ -0,0 +1,18 @@
+package academy.devdojo.springboot2essentials.domain;
+
+public class Anime {
+
+    private String name;
+
+    public Anime(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
